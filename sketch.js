7709267265d9fee9f84abbba822b64bb0994469e
euamandatrombelli10 <amanda.trombelli.assis@escola.pr.gov.br>
2024-08-20 12:48:28 -0300

    Adicionando arquivos js e css

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+    createCanvas(1000, 1000);
+      background("black");
+  }
+  
+  function draw() {
+    stroke("blue");
+    fill("red");
+    if(mouseIsPressed){
+    rect(mouseX, mouseY,20, 20)
+   }
+  
+    
+  }
